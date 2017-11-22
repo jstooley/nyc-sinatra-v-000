d9dd6f67d5d3492c3af3fdff3aa96acd8441d10e
Justin Tooley <jstooley823@gmail.com>
2017-11-22 17:56:30 +0000

    fixed some db bugs

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,5 +1,5 @@
 class CreateLandmarks < ActiveRecord::Migration
-  create_table :landmark do |t|
+  create_table :landmarks do |t|
     t.string :name
     t.integer :figure_id
     t.integer :year_completed
