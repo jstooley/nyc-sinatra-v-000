f0ad0797b47b055325b5e31923b4b82cfa683e2f
Justin Tooley <jstooley823@gmail.com>
2017-11-22 19:02:40 +0000

    added to new controller

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,7 +9,32 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    binding.pry
+
+    @figure = Figure.create(name: params["figure"]["name"])
+
+    if params["figure"]["title_ids"] != nil #checks that they checked a title box
+      params["figure"]["title_ids"].each do |title_id|
+        FigureTitle.create(figure_id: @figure.id, title_id: title_id.to_i)
+      end
+    end
+
+    if params["title"]["name"] != "" #checks that they filled in a new title
+      title = Title.find_or_create_by(name: params["title"]["name"])
+      FigureTitle.create(figure_id: @figure.id, title_id: title.id)
+    end
+
+    if params["figure"]["landmark_ids"] != nil #checks that they checked a landmark box
+      params["figure"]["landmark_ids"].each do |landmark_id|
+         landmark = Landmark.find_by(id: landmark_id.to_i)
+         landmark.figure = @figure
+      end
+    end
+
+    if params["landmark"]["name"] != "" #checks that they filled in a new landmark
+      landmark = Landmark.find_or_create_by(name: params["landmark"]["name"])
+      landmark.figure = @figure
+    end
+
     redirect to 'figures/show'
   end
 
--- db/migrate/20171122172207_create_figure_titles.rb
+++ db/migrate/20171122172207_create_figure_titles.rb
@@ -2,7 +2,7 @@ class CreateFigureTitles < ActiveRecord::Migration
   def change
     create_table :figure_titles do |t|
       t.integer :figure_id
-      t.integer :titles_id
+      t.integer :title_id
       t.timestamps null: false
     end
   end
